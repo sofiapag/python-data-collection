924bc22f261c43571d5edf82bcfc9d02534b4c69
Jerry Wu <nchannelmosfet@gmail.com>
2023-02-02 16:53:24 -0700

    updated test_crud_crops

--- test_main.py
+++ test_main.py
@@ -37,9 +37,37 @@ def create_crop(client: TestClient) -> dict:
     return response.json()
 
 
+def create_crop_400_error(client: TestClient) -> dict:
+    response = client.post(
+        "/crops", 
+        json={
+            "year": 2000,
+            "crop_type": "corn",
+            "tilled": False,
+            "tillage_depth": 2,
+            "comments": "Corn was planted",
+            "user_id": 1
+        }
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    return response.json()
+
+
 def get_all_crops(client: TestClient) -> dict:
     response = client.get("/crops")
     assert response.status_code == status.HTTP_200_OK
+    return response.json()
+
+
+def get_crop(client: TestClient, crop_id: int) -> dict:
+    response = client.get(f"/crops/{crop_id}")
+    assert response.status_code == status.HTTP_200_OK
+    return response.json()
+
+
+def get_crop_404_error(client: TestClient, crop_id: int) -> dict:
+    response = client.get(f"/crops/{crop_id}")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
     return response.json()
 
 
@@ -48,7 +76,7 @@ def delete_crop(client: TestClient, crop_id: int):
     assert response.status_code == status.HTTP_204_NO_CONTENT
 
 
-def delete_crop_404(client: TestClient, crop_id: int):
+def delete_crop_404_error(client: TestClient, crop_id: int):
     response = client.delete(f"/crops/{crop_id}")
     assert response.status_code == status.HTTP_404_NOT_FOUND
 
@@ -79,12 +107,21 @@ def test_crud_crops():
         # create an entry
         response = create_crop(client)
         crop_id = response["id"]
-        # get entry
+        # get all entries
         response = get_all_crops(client)
         assert len(response) == 1
         assert response[0]["year"] == 2000
         assert response[0]["crop_type"] == "corn"
+
+        # get 1 entry
+        response = get_crop(client, crop_id)
+        assert response["year"] == 2000
+        assert response["crop_type"] == "corn"
+
         delete_crop(client, crop_id=crop_id)
         response = get_all_crops(client)
         assert len(response) == 0
-        delete_crop_404(client, crop_id=crop_id)
+
+        delete_crop_404_error(client, crop_id=crop_id)
+        create_crop_400_error(client)
+        get_crop_404_error(client, crop_id)
